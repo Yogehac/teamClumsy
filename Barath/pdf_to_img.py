8dbbc08923c1ec5870bb5fca902e40d9d2739f9e
Yogeshwaran Arumugam <yogeshwaranarumgam@gmail.com>
2022-04-24 19:55:10 +0530

    added another ver

--- Barath/pdf_to_img.py
+++ Barath/pdf_to_img.py
@@ -9,3 +9,21 @@ for i in range(len(images)):
 
 	# Save pages as images in the pdf
 	images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+
+	
+# new method
+import pypdfium2 as pdfium
+
+pdffile = 'MailAttachments/QUOTATION 6680 [ KOTHARI SUGAR & CHEMICAL 19B ]511.pdf'
+
+
+def main():
+
+    # render multiple pages concurrently (in this case: all)
+    for image, suffix in pdfium.render_pdf_topil(pdffile):
+        image.save('output_%s.jpg' % suffix)
+
+    # render a single page (in this case: the first one)
+    with pdfium.PdfContext(pdffile) as pdf:
+        image = pdfium.render_page_topil(pdf, 0)
+        image.save('output.jpg')
